5d87764e38d2ec9d4e29d86c3ea61ba4e200a5c3
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2025-03-06 21:44:01 -0500

    add ChessLogic.Tests.Unit project

---
+++ tests/ChessLogic.Tests.Unit/BoardTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+
+namespace ChessLogic.Tests.Unit;
+public class BoardTests
+{
+    private readonly Board _sut;
+    public BoardTests()
+    {
+        _sut = new Board();
+    }
+
+    [Fact]
+    public void IsInside_ShouldReturnTrue_WhenCasillaIsInsideBoard()
+    {
+        // Arrange
+        int fila = Random.Shared.Next(0, 7);
+        int col = Random.Shared.Next(0, 7);
+        var square = new Square(fila, col);
+
+        // Act
+        var result = Board.IsInside(square);
+
+        // Asert
+        result.Should().BeTrue();
+    }
+
+
+    [Fact]
+    public void IsInside_ShouldReturnFalse_WhenCasillaIsOutsideBoard()
+    {
+        // Arrange
+        int fila = Random.Shared.Next(8, 10);
+        int col = Random.Shared.Next(8, 10);
+        var square = new Square(fila, col);
+
+        // Act
+        var result = Board.IsInside(square);
+
+        // Asert
+        result.Should().BeFalse();
+    }
+}
